21fa893233c9747259f8c72946c10d3c29d33f30
harshmax <117888618+Harsh-cyber005@users.noreply.github.com>
2024-12-19 14:04:51 +0530

    resume-data.tsx (1 file changed, 1 insertion(+), 1 deletion(-))

--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -28,7 +28,7 @@ export const RESUME_DATA = {
     "Experienced Full Stack Developer with a strong focus on backend development, DevOps, networking, and system design, committed to delivering high-quality, robust solutions.",
   summary:
     "As a Full Stack Engineer, I specialize in taking products from concept to launch, with a focus on backend development, DevOps, networking, and system design. I work primarily with TypeScript, React, Node.js, and develop robust REST APIs. My experience comes from intensive personal projects, where I’ve honed my skills in building efficient, scalable, and secure systems.",
-  avatarUrl: ".",
+  avatarUrl: "../",
   personalWebsiteUrl: "https://harshmax.vercel.app/",
   contact: {
     email: "harshxgupta931@gmail.com",
